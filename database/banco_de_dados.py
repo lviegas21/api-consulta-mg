881049385a8c3b6f1b7cca0fa560c3b6bb17f4e7
LUCAS VIEGAS <lucas.viegas006@gmail.com>
2022-10-23 12:39:52 -0300

    subindo projeto

---
+++ database/banco_de_dados.py
@@ -0,0 +1,19 @@
+class BancoDeDados:
+    def __init__(self):
+        pass
+    def connectar(self):
+        pass
+    def consulta_todos_clientes(self):
+        pass
+
+    def consulta_cliente_especifico(self):
+        pass
+
+    def deleta_cliente_especifico(self):
+        pass
+
+    def inserir_cliente(self):
+        pass
+
+    def atualizar_cliente(self):
+        pass
